8fa66a9c162b4354ed9cafa1dbe26e4e824c3a0e
Ruud <ruud.prein@gmail.com>
2016-09-02 21:30:38 +0200

    add attributes

--- src/main/scala/example/Example.scala
+++ src/main/scala/example/Example.scala
@@ -6,9 +6,18 @@ import scala.scalajs.js.annotation.{JSExport, ScalaJSDefined}
 @JSExport
 @ScalaJSDefined
 class MyExample extends js.Object {
+
   val is: String = "scala-js-polymer-app"
+
+  def properties = js.Dynamic.literal(
+    "prop1" -> js.Dynamic.literal(
+      "type" -> "String",
+      "value" -> "Ruud I am"
+    )
+  )
 
   def beforeRegister(): Unit = {
     println("beforeRegister executed in ScalaJS")
   }
+
 }
