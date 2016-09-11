8bdca459b62582f6ef6137bea48dd28a63b8dfd2
Ruud <ruud.prein@gmail.com>
2016-09-11 15:47:14 +0200

    Prevent boilerplate

--- src/main/scala/example/polymer/PolymerBase.scala
+++ src/main/scala/example/polymer/PolymerBase.scala
@@ -5,7 +5,7 @@ import scala.scalajs.js.annotation.{JSExport, JSExportDescendentObjects, ScalaJS
 
 @js.native
 object Polymer extends PolymerBase {
-  def apply(prototype: js.Any): PolymerBase = js.native
+  def apply(prototype: js.Any): PolymerClass = js.native
 }
 
 @ScalaJSDefined
@@ -16,8 +16,11 @@ trait PolymerElement extends js.Object {
 
 @js.native
 trait PolymerBase extends js.Object {
-  val properties: js.Dynamic = js.native
+  def properties: js.Dynamic = js.native
 }
+
+@js.native
+trait PolymerClass extends PolymerBase with PolymerElement
 
 @JSExportDescendentObjects
 trait PolymerComponent {
